ee87c07e8d4386fbfa463e9c609b2729dabf49b9
vamosmarton <vammarton@gmail.com>
2022-10-08 15:18:02 +0200

    add register_form.php

---
+++ Dokumentumok/Kódolás/register_form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Regisztráció</title>
+</head>
+<body>
+    <h1>Regisztráció</h1>
+    <table class="logreg">
+        <form action="register.php" method="POST" enctype="multipart/form-data">
+            <tr>
+                <td>Felhasználónév*:</td>
+                <td><input type="text" name="felhasznalonev" placeholder="valaki123" required/></td>
+            </tr>
+            <tr>
+                <td>Név:</td>
+                <td><input type="text" name="nev" placeholder="Teljes Név"/></td>
+            </tr>
+            <tr>
+                <td>Jelszó*:</td>
+                <td><input type="password" name="jelszo" required/></td>
+            </tr>
+            <tr>
+                <td>Email*:</td>
+                <td><input type="email" name="email" placeholder="valaki@email.com" requiered/></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="submit" value="Regisztrálok"/></td>
+            </tr>
+        </form>
+    </table>
+</body>
+</html>
