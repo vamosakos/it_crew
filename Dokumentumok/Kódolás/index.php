dcaf04a1d5e7707d6e95508ed96d87a89fb45815
vamosmarton <vammarton@gmail.com>
2022-10-08 14:56:35 +0200

    add index.php

---
+++ Dokumentumok/Kódolás/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Főoldal</title>
+</head>
+<body>
+    <?php
+        session_start();
+    ?>
+    <div class="menu">
+        <ul><?php require "menu.php";?></ul>
+    </div>
+    <div class="content">
+       <?php require "content.php"; ?>
+    </div>
+</body>
+</html>
